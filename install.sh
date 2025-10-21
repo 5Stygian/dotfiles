595ffd690b81c0c102476ac657f713ed40926f42
5Stygian <rlcrafistohard@gmail.com>
2025-10-20 19:00:34 -0700

    actually copy the dotfiles

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ install() {
     git clone https://github.com/5Stygian/dotfiles.git
     cd dotfiles
     echo "Copying dotfiles..."
-    scp -v ~/coding_projects/dotfiles/.bash_profile
-    scp -v ~/coding_projects/dotfiles/.bashrc
-    scp -v ~/coding_projects/dotfiles/.nanorc
+    scp -v ~/coding_projects/dotfiles/.bash_profile ~
+    scp -v ~/coding_projects/dotfiles/.bashrc ~
+    scp -v ~/coding_projects/dotfiles/.nanorc ~
     cd ~
     echo "Sourcing..."
     source ~/.bashrc
